a7cce29e39e42fde857648a2f524304dd336ac21
defqx <defqx@github.com>
2018-05-23 16:22:41 +0800

    test

--- app/src/main/java/cn/hitftcl/wearablepc/MainActivity.java
+++ app/src/main/java/cn/hitftcl/wearablepc/MainActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    int a =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
